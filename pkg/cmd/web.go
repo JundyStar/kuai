8904fd497215034498b89b4745b1ec54c81decba
JundyStar <jundy12@163.com>
2025-11-28 09:04:05 +0800

    feat: 默认监听所有网络接口，支持通过 IP 地址访问
    - 将默认 host 从 localhost 改为 0.0.0.0
    - 添加 getLocalIPs() 函数自动获取本机 IP 地址
    - 启动时显示可通过的 IP 地址列表，方便从其他设备访问

--- pkg/cmd/web.go
+++ pkg/cmd/web.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 
 	"github.com/spf13/cobra"
@@ -21,14 +22,43 @@ func newWebCmd() *cobra.Command {
 			server := web.NewServer(templateMgr, paths)
 			addr := fmt.Sprintf("%s:%d", host, port)
 			fmt.Fprintf(cmd.OutOrStdout(), "🚀 Kuai Web 界面已启动\n")
-			fmt.Fprintf(cmd.OutOrStdout(), "📱 访问地址: http://%s\n", addr)
-			fmt.Fprintf(cmd.OutOrStdout(), "按 Ctrl+C 停止服务器\n\n")
+			fmt.Fprintf(cmd.OutOrStdout(), "📱 访问地址: http://%s:%d\n", host, port)
+			
+			// 如果监听所有接口，显示本机 IP 地址
+			if host == "0.0.0.0" || host == "" {
+				if ips := getLocalIPs(); len(ips) > 0 {
+					fmt.Fprintf(cmd.OutOrStdout(), "\n💡 可通过以下地址访问:\n")
+					for _, ip := range ips {
+						fmt.Fprintf(cmd.OutOrStdout(), "   http://%s:%d\n", ip, port)
+					}
+				}
+			}
+			
+			fmt.Fprintf(cmd.OutOrStdout(), "\n按 Ctrl+C 停止服务器\n\n")
 			return http.ListenAndServe(addr, server)
 		},
 	}
 
 	webCmd.Flags().IntVarP(&port, "port", "p", 8080, "服务器端口")
-	webCmd.Flags().StringVar(&host, "host", "localhost", "服务器地址")
+	webCmd.Flags().StringVar(&host, "host", "0.0.0.0", "服务器地址 (0.0.0.0 表示监听所有网络接口)")
 	return webCmd
+}
+
+// getLocalIPs 获取本机的非回环 IP 地址列表
+func getLocalIPs() []string {
+	var ips []string
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ips
+	}
+	
+	for _, addr := range addrs {
+		if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				ips = append(ips, ipNet.IP.String())
+			}
+		}
+	}
+	return ips
 }
 
